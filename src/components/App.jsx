a45ec5cbb304516ebbaa758719958fc8b6971af7
Leonid Haidash <Gaydashleo@gmail.com>
2022-09-15 19:11:10 +0300

    ContactFormStart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import {nanoid} from 'nanoid';
+
 export const App = () => {
   return (
     <div
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from 'components/ContactForm.module.css';
+
+export default class ContactForm extends Component {
+  state = {
+    name: '',
+    number:'',
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+   
+    
+  
+}
